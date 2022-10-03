a2640b6a8426a3889783647c7253641172c16ea7
Wang, ChenYu git config --global  git config --global user.email <cwang929@insite.4cd.edu>
2022-10-03 16:21:24 -0700

    Q2

--- q2.py
+++ q2.py
@@ -8,3 +8,14 @@
 # No need to use list
 # All input values are taken one by one separatively.
 ###
+long = "a"
+shor = "asdiugfqowudiqyhvwpfiquwjhdihv"
+while True:
+ string = str(input()) 
+ if string == "stop" or string == "STOP":
+    break
+ if len(string)> len(long):
+    long = string
+ if len(string)< len(shor):
+    shor = string
+print(long+" " +shor)
